bb710bb657f338945f66e3c0a53ecfd8bb1a644d
Dima Denysenko <coding@dimdnk.com>
2022-05-16 16:17:43 +0300

    fix: declare sequence in lowercase

--- src/main/java/org/ujar/basics/rest/userprofilecrud/entity/UserProfile.java
+++ src/main/java/org/ujar/basics/rest/userprofilecrud/entity/UserProfile.java
@@ -17,16 +17,16 @@ import lombok.NoArgsConstructor;
 @Table(name = UserProfile.TABLE_NAME)
 public class UserProfile {
 
-  protected static final String TABLE_NAME =  "BASICS_REST_USER_PROFILES";
+  protected static final String TABLE_NAME =  "basics_rest_user_profiles";
 
   @Id
   @SequenceGenerator(
-      name = "USER_PROFILE_ID_SEQ",
-      sequenceName = "USER_PROFILE_ID_SEQ"
+      name = "user_profile_id_seq",
+      sequenceName = "user_profile_id_seq"
   )
   @GeneratedValue(
       strategy = GenerationType.SEQUENCE,
-      generator = "USER_PROFILE_ID_SEQ"
+      generator = "user_profile_id_seq"
   )
   private Long id;
   private boolean active;
