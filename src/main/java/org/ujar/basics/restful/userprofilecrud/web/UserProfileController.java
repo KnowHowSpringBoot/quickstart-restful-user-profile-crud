e5d27f0e1dd1ace2da02b2f6dc90960def7066bb
Dima Denysenko <coding@dimdnk.com>
2022-07-23 11:46:34 +0300

    feat: move dto to web package

--- src/main/java/org/ujar/basics/restful/userprofilecrud/web/UserProfileController.java
+++ src/main/java/org/ujar/basics/restful/userprofilecrud/web/UserProfileController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.ujar.basics.restful.userprofilecrud.dto.UserProfileDto;
+import org.ujar.basics.restful.userprofilecrud.web.dto.UserProfileDto;
 import org.ujar.basics.restful.userprofilecrud.entity.UserProfile;
 import org.ujar.basics.restful.userprofilecrud.repository.UserProfileRepository;
 import org.ujar.boot.starter.restful.web.dto.ErrorResponse;
