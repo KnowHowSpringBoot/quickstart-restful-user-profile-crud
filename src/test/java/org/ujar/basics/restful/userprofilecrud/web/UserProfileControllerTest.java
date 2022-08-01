718485aeb5671f3f717506619369a946ae842e95
Dima Denysenko <coding@dimdnk.com>
2022-08-01 14:19:21 +0300

    feat: change snapshot version to 0.9.0

--- src/test/java/org/ujar/basics/restful/userprofilecrud/web/UserProfileControllerTest.java
+++ src/test/java/org/ujar/basics/restful/userprofilecrud/web/UserProfileControllerTest.java
@@ -10,19 +10,16 @@ import com.google.gson.Gson;
 import java.util.ArrayList;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.ujar.basics.restful.userprofilecrud.entity.UserProfile;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class UserProfileControllerTest {
